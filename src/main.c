427f2326ca23d47fa126330d733b24bec836210f
sudosmile <mael.leclerc@epitech.eu>
2021-05-14 21:21:17 +0200

    {modified} made variable const

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 static void print_frame(int frame)
 {
-    int start_of_frame = (LENGTH * WIDTH * frame);
+    const int start_of_frame = (LENGTH * WIDTH * frame);
     int end_of_frame = (LENGTH * WIDTH * (frame + 1));
 
     for (int i = start_of_frame; i < end_of_frame; i++) {

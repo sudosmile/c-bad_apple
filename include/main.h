318740059a0649d15c3468222cbbc8e62884f69d
sudosmile <mael.leclerc@epitech.eu>
2021-05-15 01:10:04 +0200

    {modified} removed cursor and handled Cc signal to turn it back on

--- include/main.h
+++ include/main.h
@@ -2,12 +2,15 @@
 #define MAIN_H_
 
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
 
 enum constants {
     SUCCESS = 0,
-    FPS = 24,
+    FPS = 30,
     TOTAL_FRAMES = 6569,
     LENGTH = 24,
     WIDTH = 80
--- src/main.c
+++ src/main.c
@@ -1,15 +1,28 @@
 #include "main.h"
 #include "frames.h"
+#include "../include/main.h"
+#include "../include/frames.h"
+
+volatile bool keepRunning = true;
+
+void intHandler(int _)
+{
+    (void)_;
+    keepRunning = false;
+    puts(" stopped by signal");
+}
 
 static void print_frame(int frame)
 {
-    const int start_of_frame = (LENGTH * WIDTH * frame);                        // start of frame in the frames string
+    const int start_of_frame = (LENGTH * WIDTH * frame);
     const int end_of_frame = (LENGTH * WIDTH * (frame + 1));
+    const int length_of_frame = end_of_frame - start_of_frame;
+    char substr_frame[LENGTH * WIDTH];
 
-    puts("\033[H");                                                             // reset cursor to 0 0
-    for (int i = start_of_frame; i < end_of_frame; i++) {
-        putchar(all_frames[i]);
-    }
+    strncpy(substr_frame, all_frames + start_of_frame, length_of_frame);
+    substr_frame[length_of_frame] = '\0';
+    puts("\033[H");
+    fputs(substr_frame, stdout);
 }
 
 int main(void)
@@ -19,18 +32,23 @@ int main(void)
     int time_taken = 0;
     struct timespec to_wait;
     struct timespec handler;
+    struct sigaction act = {0};
 
-    puts("\033[2J");                                                            // clear terminal
+    act.sa_handler = intHandler;
+    sigaction(SIGINT, &act, NULL);
+    puts("\033[2J");
     puts("\033[H");
-    while (current_frame <= TOTAL_FRAMES) {
+    puts("\e[?25l");//remove cursor
+    while (keepRunning && current_frame <= TOTAL_FRAMES) {
         start_frame = time(NULL);
         print_frame(current_frame);
         time_taken = start_frame - time(NULL);
         to_wait.tv_sec = 0;
-        to_wait.tv_nsec = (1000000000 / FPS) - time_taken;                      // set nanosecond wait to slow some frames to fit framerate
+        to_wait.tv_nsec = (1000000000 / FPS) - time_taken;
         if (to_wait.tv_nsec > 0)
             nanosleep(&to_wait, &handler);
         current_frame++;
     }
+    printf("\e[?25h");
     return SUCCESS;
 }
